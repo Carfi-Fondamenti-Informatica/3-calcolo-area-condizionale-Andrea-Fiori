5c89d32c6c4bbc2948ca62d796ba689d487bc1cf
Andrea-Fiori <115191307+Andrea-Fiori@users.noreply.github.com>
2022-11-17 19:39:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,25 +2,26 @@
 using namespace std;
 
 int main()
-float a=0, b=0;
-int opzione=0;
-cin >> a;
-cin >> b;
-cin >> opzione;
-switch (opzione)
 {
-    case 0:
-      cout << (b*a)/2 << endl;
-      break;
-    case 1:
-      cout << a*a << endl;
-      break;
-    case 2:
-      cout << a*b << endl;
-      break;
-   default:
-      cout << "opzione non valida" << endl;
-}
+  float a=0, b=0;
+  int opzione=0;
+  cin >> a;
+  cin >> b;
+  cin >> opzione;
+  switch (opzione)
+    {
+      case (0):
+        cout << (b*a)/2 << endl;
+        break;
+      case (1):
+        cout << a*a << endl;
+        break;
+      case (2):
+        cout << a*b << endl;
+        break;
+     default:
+        cout << "opzione non valida" << endl;
+    }
    
    
    
